149cf2cfe5d856d80dbfb8e07c52a894451f7064
Brad Newman <newmanbradm@gmail.com>
2018-05-20 20:22:08 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esperanto_array = esperanto_alphabet.split("")
+  arr.sort_by do |string|
+    string.split("").collect do |letters|
+      esperanto_array.index(letters)
+    end
+  end
 end
