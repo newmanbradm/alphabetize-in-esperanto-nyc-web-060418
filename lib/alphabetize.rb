de19e28330300e9fb88cbef91189380ef33a219e
Brad Newman <newmanbradm@gmail.com>
2018-05-20 20:22:00 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,5 @@
-esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-
 def alphabetize(arr)
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   esperanto_array = esperanto_alphabet.split("")
   arr.sort_by do |string|
     string.split("").collect do |letters|
