cdc802de968b53ac5291b5d0ee6c77499aeb9ac9
Brad Newman <newmanbradm@gmail.com>
2018-05-20 20:20:31 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  esperanto_array = esperanto.split("")
+  esperanto_array = ESPERANTO_ALPHABET.split("")
   arr.sort_by do |string|
     string.split("").collect do |letters|
       esperanto_array.index(letters)
